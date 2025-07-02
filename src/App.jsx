f2a3e49380219860623f6b059e4359430b1cc18e
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-07-02 10:22:55 -0500

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './page/home'
+import Home from './page/Home'
 import Movies from './page/Movies'
 import Header from './components/Header'
 import Footer from './components/Footer'
