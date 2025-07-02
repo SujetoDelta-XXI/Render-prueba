f510b27947c0f16483859401ad276853351279fb
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-07-02 10:19:40 -0500

    de

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './page/home'
-import Movies from './page/movies'
+import Movies from './page/Movies'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
